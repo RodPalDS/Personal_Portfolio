635dc99ec489ee64cf5efe03a2b0627325f6f6c0
RodPalDS <rodrigopalpa@gmail.com>
2025-05-05 17:04:27 -0300

    agregando_link_CV

--- curriculum/urls.py
+++ curriculum/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='inicio'),
+    path('cv/', views.cv, name='cv'),
 ]
--- curriculum/views.py
+++ curriculum/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def index(request):
     print("Cargando vista index")
     return render(request, 'curriculum/index.html')
+
+
+def cv(request):
+    return render(request, 'curriculum/cv.html')
